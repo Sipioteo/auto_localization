06eb4eb6028840f50f9af07822e83ee0410bae16
SipioTeo <matteo@sipio.it>
2020-11-24 22:17:14 +0100

    Updated

--- lib/auto_localization.dart
+++ lib/auto_localization.dart
@@ -7,6 +7,14 @@ import 'package:translator/translator.dart';
 
 import 'dart:ui' as ui;
 
+import 'package:license/license.dart';
+
+void main() {
+  final licenseData = LicenseData(copyrightHolderName: 'John Brown');
+  print(mit(licenseData));
+}
+
+
 class BaseLanguage{
 
   static final BaseLanguage _singleton = new BaseLanguage._internal();
